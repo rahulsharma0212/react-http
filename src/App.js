6c5e403fb34927a6ce657b35c5d1f8423a862682
rahul.sharma <rahul.sharma@mapmyindia.com>
2022-09-29 17:41:16 +0530

    lazy loading react above 16.6 init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-import Blog from './containers/Blog/Blog';
+import Posts from './containers/Posts';
+import User from './containers/User';
+import Welcome from './containers/Welcome';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <nav>
+            <NavLink to="/user">User Page</NavLink> |&nbsp;
+            <NavLink to="/posts">Posts Page</NavLink>
+          </nav>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/user" component={User} />
+          <Route path="/posts" component={Posts} />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
